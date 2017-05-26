0f9cf3958aebf09cfee7bc529e04716fac126a02
DESKTOP-HVP6MPC\Chris <chris@thecarmans.com>
2017-05-26 12:55:01 +0200

    opgave 1

--- vandmelon.py
+++ vandmelon.py
@@ -15,14 +15,32 @@ def importCSV(directory,ext):
 			csv[x[:-4]] = np.genfromtxt(x, delimiter=",")
 			files.append(x)
 	print('The following files have been imported: \n' + str(files)+'\n')
+def f1(t,f,tau):
+	return np.exp(-t/tau)*np.sin(2*np.pi*f*t)
 
 importCSV(currentDir,extension)
 
 print('####################################')
 print('Welcome to opgave 1')
 print('####################################\n')
+f = 0.629
+tau = 19.0
+time = csv['exc1'][1:,0]
+signal = csv['exc1'][1:,1]
+error = csv['exc1'][1:,2]
+print('Here we have f = '+str(f)+' and t = '+str(tau)+'\n')
 
-
+print('generating plot!')
+plt.figure(1)
+line2, = plt.plot(time,f1(time,f,tau), label = "Theoretical Model")
+line1 = plt.errorbar(time, signal,yerr = error, fmt='o', label = 'Raw Data')
+plt.title('Signal as a function of time')
+plt.xlabel('Time [s]')
+plt.ylabel('Signal [mV]')
+first_legend = plt.legend(handles = [line1], loc=1)
+ax = plt.gca().add_artist(first_legend)
+second_legend = plt.legend(handles = [line2], loc=4)
+plt.show()
 print('####################################')
 print('Welcome to opgave 2')
 print('####################################\n')
